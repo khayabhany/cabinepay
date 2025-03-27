f6c8e9f96ca997e0ce5364abad3f6e5cf3cfc362
khayabhany <kamissahjeanbaptiste@gmail.com>
2025-03-27 02:20:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+{
+    "name": "CabinePay",
+    "short_name": "CabinePay",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#FFFFFF",
+    "theme_color": "#007BFF",
+    "description": "Recharge mobile instantanée",
+    "icons": [
+        {
+            "src": "/img/logo-192.png",
+            "type": "image/png",
+            "sizes": "192x192",
+            "purpose": "any maskable"
+        },
+        {
+            "src": "/img/logo-512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+        }
+    ]
+}
